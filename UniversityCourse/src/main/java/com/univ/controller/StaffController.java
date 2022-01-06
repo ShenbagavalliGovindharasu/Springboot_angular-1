32656cb11d27a8cba52d02e6cf546fbcb8786759
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2022-01-06 10:42:18 +0530

    first commit

---
+++ UniversityCourse/src/main/java/com/univ/controller/StaffController.java
@@ -0,0 +1,77 @@
+package com.univ.controller;
+
+import java.util.List;
+import javax.validation.Valid;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.univ.entity.Staff;
+import com.univ.repository.StaffRepository;
+
+@RestController
+@RequestMapping("/univ")
+public class StaffController {
+	
+	
+	@Autowired
+	public StaffRepository staffRepository;
+	
+	@GetMapping(path="/display")
+	public String display() {
+		return "hello";
+	}
+	
+	//display all staff records
+	@GetMapping("/getallstaff")
+	public List<Staff> getAllStaff(){
+		return staffRepository.findAll();
+	}
+	
+	//save staff record
+	@PostMapping(path="/savestaff",consumes="application/json")
+    public Staff createStaff(@Valid @RequestBody Staff staff) {
+        return staffRepository.save(staff);
+    }
+
+	
+	
+	//update staff
+	@PutMapping("/updatestaff")
+	public Staff update(@RequestBody Staff staff) {
+		return staffRepository.save(staff);
+	}
+	
+	//delete staff using id
+	@RequestMapping(value="/deletestaff/{id}",method=RequestMethod.DELETE)
+    public void  deleteStaff(@PathVariable("id") long id) {
+
+    staffRepository.deleteById(id);
+
+    }
+	
+	@PutMapping("/updatebyid/{id}")
+	public Staff updateNote(@PathVariable(value = "id") Long staffId,
+	                                        @Valid @RequestBody Staff staffDetails) {
+
+	   Staff staff = staffRepository.findById(staffId).orElseThrow();
+		
+	    staff.setId(staffDetails.getId());
+	    staff.setStaffname(staffDetails.getStaffname());
+	    staff.setCourse(staffDetails.getCourse());
+	   
+	    Staff updatedstaff = staffRepository.save(staff);
+	    return updatedstaff;
+	}
+	
+	
+	
+}
